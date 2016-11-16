92583a13dca6854f55e74c4f643e0a320656a991
Garrett <tbucketg@gmail.com>
2016-11-15 20:50:31 -0600

    Asg 10 - Asynch Decorator

---
+++ CurrencyTrader.AdoNet/AsynchTradeStorage.cs
@@ -0,0 +1,26 @@
+﻿using CurrencyTrader.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyTrader.AdoNet
+{
+    public class AsynchTradeStorage : ITradeStorage
+    {
+        private readonly ILogger logger;
+        AdoNetTradeStorage tradeStorage;
+
+        public AsynchTradeStorage(ILogger logger)
+        {
+            this.logger = logger;
+            tradeStorage = new AdoNetTradeStorage(logger);
+        }
+
+        public void Persist(IEnumerable<TradeRecord> trades)
+        {
+            Task.Run(() => tradeStorage.Persist(trades));
+        }
+    }
+}
